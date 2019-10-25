6b27ec56a8dbba7b368475a009b221a1eb9829c5
Prendergast1 <47788939+Prendergast1@users.noreply.github.com>
2019-10-24 21:48:36 -0400

    Update main.c

--- Software PWM/MSP430G2553/main.c
+++ Software PWM/MSP430G2553/main.c
@@ -1,7 +1,7 @@
 // Author: Sean Prendergast
 // Code was initially started on September 18th, 2019
 // Code was last modified on October 15th, 2019
-// Submitted October 3rd, 2019
+// Submitted October 15th, 2019
 // Exercise: Software PWM G2553
 
 #include <msp430.h>
